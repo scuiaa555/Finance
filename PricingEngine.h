1068bcb0169fb6599bd5a45c3415473816fcd67b
scuiaa <cuishidong555@gmail.com>
2016-06-27 21:12:02 +0800

    pricing engine

--- PricingEngine.h
+++ PricingEngine.h
@@ -5,9 +5,31 @@
 #ifndef FINANCE_PRICING_ENGINE_H
 #define FINANCE_PRICING_ENGINE_H
 
+#include <memory>
+#include "nameDef.h"
 
 class PricingEngine {
+public:
+    virtual void calculate() = 0;
 
+    class Results {
+    public:
+        Money price;
+    };
+};
+
+class AnalyticBSEngine : public PricingEngine {
+public:
+    void calculate() override;
+
+    class AnalyticBSResults : public virtual PricingEngine::Results {
+    public:
+        double delta;
+    };
+
+private:
+    std::shared_ptr<Model> model_;
+    std::shared_ptr<PricingEngine::Results> results_;
 };
 
 
